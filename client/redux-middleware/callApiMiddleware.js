dd0254947e7f7eb9d7fa81cc06bec521a8816d5f
David Luu <luu.david.c@gmail.com>
2017-04-09 07:55:19 -0700

    Api Middleware async/await refactoring

--- client/redux-middleware/callApiMiddleware.js
+++ client/redux-middleware/callApiMiddleware.js
@@ -2,7 +2,8 @@ import fetch from 'isomorphic-fetch';
 
 const API_URL = '/api';
 
-function callAPI(endpoint, method = 'GET', body) {
+async function callAPI(endpoint, method = 'GET', body) {
+  /*
   return fetch(`${API_URL}/${endpoint}`, {
     headers: {
       'content-type': 'application/json',
@@ -20,6 +21,27 @@ function callAPI(endpoint, method = 'GET', body) {
     return json;
   })
   .catch(error => Promise.reject(error));
+  */
+
+  try {
+    const response = await fetch(`${API_URL}/${endpoint}`, {
+      headers: {
+        'content-type': 'application/json',
+      },
+      method,
+      credentials: 'include',
+      body: JSON.stringify(body),
+    });
+    const json = await response.json();
+
+    if (!response.ok) {
+      throw new Error(json);
+    } else {
+      return json;
+    }
+  } catch (err) {
+    return err;
+  }
 }
 
 function requestAPI(requestType) {
@@ -30,6 +52,7 @@ function requestAPI(requestType) {
 
 export const CALL_API = Symbol('CALL_API');
 
+/*
 export default store => next => (action) => {
   const request = action[CALL_API];
 
@@ -62,4 +85,38 @@ export default store => next => (action) => {
           type: errorType,
         })
     );
+};
+*/
+
+export default store => next => async (action) => {
+  const request = action[CALL_API];
+
+  if (!request) {
+    return next(action);
+  }
+
+  const { dispatch } = store;
+  const { endpoint, method, body, types } = request;
+  const [requestType, successType, errorType] = types;
+
+  if (method !== 'GET') {
+    const _csrf = store.getState()._csrf;
+    Object.assign(body, { _csrf });
+  }
+
+  dispatch(requestAPI(requestType));
+
+  try {
+    const response = await callAPI(endpoint, method, body);
+
+    return next({
+      response: response.response,
+      type: successType,
+    });
+  } catch (err) {
+    return next({
+      error: err.error || 'Error',
+      type: errorType,
+    });
+  }
 };
