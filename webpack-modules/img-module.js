41c4a4e30b4e5e59e614f58233acf08cc83b36b9
David Luu <luu.david.c@gmail.com>
2017-04-22 21:04:10 -0700

    Switched from image-webpack-loader to imagemin-webpack-plugin and disabled pngquant to fix travis build issue

--- webpack-modules/img-module.js
+++ webpack-modules/img-module.js
@@ -1,34 +1,9 @@
 module.exports = (prod) => {
   return {
     test: /\.(jpe?g|gif|png|svg)$/i,
-    use: [
-      {
-        loader: 'file-loader',
-        options: {
-          limit: 10000,
-        },
-      },
-      {
-        loader: 'image-webpack-loader',
-        options: {
-          gifsicle: {
-            optimizationLevel: prod ? 3 : 1,
-          },
-          mozjpeg: {
-            dcScanOpt: prod ? 2 : 1,
-          },
-          optipng: {
-            optimizationLevel: prod ? 7 : 3,
-          },
-          svgo: {
-            options: {},
-          },
-          pngquant: {
-            quality: '65-90',
-            speed: 4,
-          },
-        },
-      },
-    ],
+    loader: 'file-loader',
+    options: {
+      limit: 10000,
+    },
   };
 };
