e034d30e25145bd07bd6d38dcfd0b1c6a05434f4
Joseph Le <josephle@me.com>
2017-06-06 11:42:04 -0700

    Basic Challenge component

---
+++ client/modules/Challenge/Challenge.jsx
@@ -0,0 +1,101 @@
+/* eslint-disable max-len */
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+
+import Header from 'components/Header/Header';
+import Footer from 'components/Footer/Footer';
+
+import globalStyles from 'main.scss';
+import styles from './Challenge.scss';
+
+/**
+ * Challenge
+ */
+
+const propTypes = {};
+
+const contextTypes = {
+  router: PropTypes.object,
+};
+
+const defaultProps = {};
+
+class Challenges extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      challenge: {
+        id: 1,
+        name: 'Challenge 1',
+        description: 'Challenge description',
+        thumbnail: '',
+        phase: 0,
+        resources: [
+          {
+            id: 1,
+            name: 'Resource 1',
+            url: 'http://graham.net/voluptatibus-occaecati-similique-ut-omnis',
+            description: 'Resource description',
+            type: 'link',
+            phase: 0,
+          },
+        ],
+        questions: [
+          {
+            id: 1,
+            text: 'Question 1',
+            insights: [
+              {
+                text: 'Insight 1',
+              },
+            ],
+          },
+        ],
+      },
+    };
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    const challenge = this.state.challenge;
+    return (
+      <section className={styles.challenge}>
+        <Helmet
+          title={challenge.name}
+        />
+        <Header
+          backgroundImg={''}
+          headerText={challenge.name}
+          subheaderText={''}
+          showButton={false}
+        />
+        <div className={globalStyles.textContainer}>
+          <p>{challenge.name}</p>
+          <p>{challenge.phase}</p>
+          <p>{challenge.description}</p>
+          <div>
+            {challenge.resources.map(resource =>
+              <a ref={resource.id} href={resource.url}>{resource.name}</a>
+            )}
+          </div>
+          <div>
+            {challenge.questions.map(question =>
+              <p>{question.text}</p>
+            )}
+          </div>
+        </div>
+        <Footer />
+      </section>
+    );
+  }
+}
+
+Challenges.propTypes = propTypes;
+Challenges.contextTypes = contextTypes;
+Challenges.defaultProps = defaultProps;
+
+export default Challenges;
