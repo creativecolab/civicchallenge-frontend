5c33d2457b007135f7e878913910e464b23d4dfa
Joseph Le <josephle@me.com>
2017-06-14 16:42:58 -0700

    More staging environment config

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * Entry Script
  */
 
-if (process.env.NODE_ENV === 'production') {
+if (['staging', 'production'].includes(process.env.NODE_ENV)) {
   process.env.webpackAssets = JSON.stringify(require('./client-dist/manifest.json'));
   process.env.webpackChunkAssets = JSON.stringify(require('./client-dist/chunk-manifest.json'));
 
