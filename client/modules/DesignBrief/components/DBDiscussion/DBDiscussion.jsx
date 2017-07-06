fa6c9acb91a7014aee4cc3d9a7924d61d90fc9d3
Lauren Liu <lmliu@ucsd.edu>
2017-07-06 15:32:37 -0700

    text updates for design brief

--- client/modules/DesignBrief/components/DBDiscussion/DBDiscussion.jsx
+++ client/modules/DesignBrief/components/DBDiscussion/DBDiscussion.jsx
@@ -22,8 +22,10 @@ function DBDiscussion() {
       <div className={styles.discussionBox}>
         <div className={styles.discussionQBar}>
           <div className={styles.discussionQBarText}>
-            <h3>Discussion Topic</h3>
-            <h5>CURRENT</h5>
+            <h3>If we were to develop a participatory design activity that would help us learn more
+              about accessibility concerns facing real stakeholders, what are the design considerations
+              for developing an activity at specific points along the mobility infrastructure (e.g.,
+              transit centers, trolleys)?</h3>
           </div>
           <div className={styles.discussionBtn}>
             <button>+</button>
@@ -32,13 +34,14 @@ function DBDiscussion() {
         <div id="discussionAnswer" className={styles.discussionAnswerBox}>
           <div className={styles.discussionContent}>
             <div className={styles.discussionQuestion}>
-              <h4>Discussion Question</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar, lorem sit amet pellentesque accumsan,
-              massa nisl commodo dui, quis gravida enim eros ut enim. Sed rutrum ornare quam, nec suscipit nulla pulvinar
-              sollicitudin. Aenean dignissim mi augue,
-              id lobortis mauris scelerisque quis. Nam pellentesque et nulla eu egestas. Suspendisse elit ex, lobortis etå
-              pharetra facilisis, sagittis eget mi. Fusce lobortis, quam id sollicitudin imperdiet, felis sem viverra tellus,
-               at porttitor elit enim quis diam. Ut pellentesque dui ac interdum dictum.</p>
+              <h4>Key Sub Question</h4>
+              <ol>
+                <li>A new state law and MTS policy imposes a fine on passengers who do not give up
+                their seat to seniors and people with disabilities if the seat is located in a wheelchair
+                or priority seating area of the bus or Trolley. What are other ways of motivating
+                people to give up their seat to seniors and people with disabilities? Fines motivate
+                people because they are a negative incentive, what might be a positive incentive?</li>
+              </ol>
             </div>
             <div className={styles.discussionSlack}>
               <div className={styles.discussionHighlights}>
@@ -56,6 +59,27 @@ function DBDiscussion() {
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+        <div className={styles.discussionQBar}>
+          <div className={styles.discussionQBarText}>
+            <h3>Trace a hypothetical path that a person with a disability might navigate through the city,
+            for example traveling from their home to a local market. What assumptions, values or beliefs
+              do we make when constructing that hypothetical path?</h3>
+          </div>
+          <div className={styles.discussionBtn}>
+            <button>+</button>
+          </div>
+        </div>
+        <hr />
+        <div className={styles.discussionQBar}>
+          <div className={styles.discussionQBarText}>
+            <h3>In envisioning the route, it’s important to consider and factor in the challenges
+            that people with disabilities might face. How might we learn about these challenges along
+            a trip from home to somewhere familiar, like the market, or to somewhere less familiar?</h3>
+          </div>
+          <div className={styles.discussionBtn}>
+            <button>+</button>
           </div>
         </div>
       </div>
--- client/modules/DesignBrief/components/DBOverview/DBOverview.jsx
+++ client/modules/DesignBrief/components/DBOverview/DBOverview.jsx
@@ -20,14 +20,18 @@ function dbHeader() {
     <div className={styles.overview}>
       <div className={styles.overviewText}>
         <h2>The Brief</h2>
-        <p>To succeed you must believe. When you believe, you will succeed. Bless up.
-        Congratulations, you played yourself. You smart, you loyal, you a genius. They
-        never said winning was easy. Some people can’t handle success, I can. Lion!
-        The key is to enjoy life, because they don’t want you to enjoy life. I promise
-        you, they don’t want you to jetski, they don’t want you to smile. You see the
-        hedges, how I got it shaped up? It’s important to shape up your hedges, it’s like
-        getting a haircut, stay fresh. Learning is cool, but knowing is better, and I know
-        the key to success.</p>
+        <p>The lack of accessible and affordable transportation for citizens with disabilities is a
+        complex issue both in terms of economy and life quality. People with disabilities face
+        individually unique challenges. For example, a person who requires crutches, a cane, or
+        a walker to get around have completely different difficulties in winter weather from those
+        of a person in a wheelchair. Similarly, a sight-impaired person may have more trouble with
+        certain crosswalk and intersection designs than a person with good sight who uses a wheelchair.
+        Certain geographic areas in the San Diego County have lesser forms of public transportation than
+        others. Many of the transit systems are far a walking distances away, making it difficult
+        for people to access them if they are immobile. And even when they gain access to these transit
+          systems, they may find themselves posed with other problems. How can we help this group by
+        easing their mobility across the city? How can we get their input about specific spots that
+        needs improvement? Even the communication involved in accessibility may need to be improved.</p>
       </div>
       <div className={styles.overviewSideBar}>
         <div className={styles.currentPhase}>
