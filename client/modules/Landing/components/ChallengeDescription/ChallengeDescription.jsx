2b9fc7df613906edc5cc8d61d4392ff2843b7f95
choyujin95 <yujin95@uw.edu>
2017-07-07 14:42:05 -0700

    landing challenge descr

--- client/modules/Landing/components/ChallengeDescription/ChallengeDescription.jsx
+++ client/modules/Landing/components/ChallengeDescription/ChallengeDescription.jsx
@@ -27,16 +27,20 @@ function ChallengeDescription(props) {
     <section id={styles.ChallengeDescription} ref={props.componentRef}>
       <div className={globalStyles.sectionTitle}>
         <h1>The Challenge</h1>
-        <p>D4SD has partnered with the <a href="http://designlab.ucsd.edu/>">UC San Diego Design Lab</a>, <a href="http://www.designforwardsd.com/">the Design Forward Summit</a>, <a href="http://www.scalesd.com/">SCALE SD</a> to
-        host an inaugural design challenge to engage San Diego residents in human-centered design. This year’s Civic Challenge focuses on the complex issue of mobility.</p>
-        <p>The people of San Diego know the citys treasures and frustrations firsthand and have the passion
-        to shape its future. D4SD and Design Forward want to hear from you! This year, for the first time,
-        we are hosting a city-wide design challenge to amplify your voices and ideas. We invite you to share
-        your experiences, learn about those of your neighbor and work together to propose answers for some
-        of San Diegos toughest questions. The challenge is structure around design thinking--a human-centered
-        approach to innovation--and offers prizes for the brightest solutions .Join us for the 2017 Civic
-        Design Challenge to tackle the complex issue of mobility.</p>
-        <a className={styles.blueOutline} href="/howitworks">LEARN MORE</a>
+        <div className={styles.descr}>
+          <p>D4SD has partnered with the <a href="http://designlab.ucsd.edu/>">UC San Diego Design Lab</a>, <a href="http://www.designforwardsd.com/">the Design Forward Summit</a>, <a href="http://www.scalesd.com/">SCALE SD</a> to
+          host an inaugural design challenge to engage San Diego residents in human-centered design. This year’s Civic Challenge focuses on the complex issue of mobility.</p>
+          <p>The people of San Diego know the citys treasures and frustrations firsthand and have the passion
+          to shape its future. D4SD and Design Forward want to hear from you! This year, for the first time,
+          we are hosting a city-wide design challenge to amplify your voices and ideas. We invite you to share
+          your experiences, learn about those of your neighbor and work together to propose answers for some
+          of San Diegos toughest questions. The challenge is structure around design thinking--a human-centered
+          approach to innovation--and offers prizes for the brightest solutions .Join us for the 2017 Civic
+          Design Challenge to tackle the complex issue of mobility.</p>
+        </div>
+        <div className={styles.button}>
+          <a className={styles.blueOutline} href="/howitworks">HOW IT WORKS</a>
+        </div>
       </div>
     </section>
   );
