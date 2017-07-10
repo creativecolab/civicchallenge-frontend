28672a32642711ff65190a34a21813ce84302902
Lauren Liu <lmliu@ucsd.edu>
2017-07-10 14:15:46 -0700

    added header to top, added logo

--- client/modules/Landing/components/ChallengeText/ChallengeText.jsx
+++ client/modules/Landing/components/ChallengeText/ChallengeText.jsx
@@ -2,7 +2,6 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import ReactGA from 'react-ga';
 
 import styles from './ChallengeText.scss';
 
@@ -18,30 +17,9 @@ const contextTypes = {
 
 const defaultProps = {};
 
-function logClick() {
-  ReactGA.event({
-    category: 'Registration',
-    action: 'Clicked Register Today',
-    label: 'Challenge Text link',
-  });
-}
-
 function ChallengeText() {
   return (
     <section className={styles.challengeText}>
-      <div className={styles.header}>
-        <div className={styles.main}>
-          <h1 className={styles.text3}>Design for San Diego&#39;s</h1>
-          <h1 className={styles.text1}>2017 Civic Challenge</h1>
-          <h1 className={styles.text2}>Mobility</h1>
-          <p className={styles.text4}><i>Build design skills as you turn your mobility challenges into civic innovations!</i></p>
-          <div className={styles.button}>
-            <div className={styles.buttonContainer}>
-              <a onClick={logClick} href="https://docs.google.com/forms/d/e/1FAIpQLSe-DzyjLGgLHdXkc8Fj7yGJrxm-PJ2Gr_LABKGeQDH699O6Hw/viewform?usp=sf_link" rel="noopener noreferrer" target="_blank">Register Today</a>
-            </div>
-          </div>
-        </div>
-      </div>
       <div className={styles.description}>
         <p>D4SD has partnered with the <a href="http://designlab.ucsd.edu/>">UC San Diego Design Lab</a>, <a href="http://www.designforwardsd.com/">the Design Forward Summit</a>, <a href="http://www.scalesd.com/">SCALE SD</a> to
         host an inaugural design challenge to engage San Diego residents in human-centered design. This year’s Civic Challenge focuses on the complex issue of mobility.</p>
