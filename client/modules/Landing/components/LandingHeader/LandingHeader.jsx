c160d0ec63c33574c300a12a5547b086d214b6de
Lauren Liu <lmliu@ucsd.edu>
2017-07-13 11:30:12 -0700

    rollback to old header

--- client/modules/Landing/components/LandingHeader/LandingHeader.jsx
+++ client/modules/Landing/components/LandingHeader/LandingHeader.jsx
@@ -31,9 +31,10 @@ function LandingHeader() {
     <section id={styles.headerBackground}>
       <div className={styles.main}>
         <div className={styles.mainContent}>
-          <p className={styles.text4}>Design for San Diego&#39;s 2017 Civic Challenge</p>
+          <h1 className={styles.text3}>Design for San Diego&#39;s</h1>
+          <h1 className={styles.text1}>2017 Civic Challenge</h1>
           <h1 className={styles.text2}>Mobility</h1>
-          <p className={styles.text3}>Build design skills as you imagine a San Diego where we all move freely</p>
+          <p className={styles.text4}><i>Build design skills as you turn your mobility challenges into civic innovations!</i></p>
           <div className={styles.button}>
             <div className={styles.buttonContainer}>
               <a onClick={logClick} href="https://docs.google.com/forms/d/e/1FAIpQLSe-DzyjLGgLHdXkc8Fj7yGJrxm-PJ2Gr_LABKGeQDH699O6Hw/viewform?usp=sf_link" rel="noopener noreferrer" target="_blank">Register Today</a>
