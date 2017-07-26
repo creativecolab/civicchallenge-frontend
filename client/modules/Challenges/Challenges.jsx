625a2c721dc747be2b8ea5e29daac4d6185b49b4
choyujin95 <yujin95@uw.edu>
2017-07-26 16:07:22 -0700

    css changes

--- client/modules/Challenges/Challenges.jsx
+++ client/modules/Challenges/Challenges.jsx
@@ -29,7 +29,7 @@ import walking from './walking.png';
 
 const INFO = [
   {
-    sectionTitle: 'The Challenge Briefs',
+    sectionTitle: 'Challenge Briefs',
     text: 'D4SD is collaborating with the Design Forward Alliance to host a 2017 large-scale human-centered design challenge around the complex civic issue of Mobility. People need to move around a city. Whether for work or play—and by car, bike, train, bus, boat, or foot— mobility significant affects the daily lives of millions. San Diego is culturally and economically diverse, as well as geographical dispersed, which makes it a ripe topic for exploring issues around transportation. The mobility challenges below investigate concerns currently facing San Diegans, as well as take a look to future transportation systems.',
   },
 ];
--- client/modules/GetInvolved/GetInvolved.jsx
+++ client/modules/GetInvolved/GetInvolved.jsx
@@ -10,15 +10,15 @@ import StandardSectionBox from 'components/StandardSectionBox/StandardSectionBox
 import globalStyles from 'main.scss';
 
 /* import GetInvolvedHeader from './components/GetInvolvedHeader/GetInvolvedHeader'; */
-import GetInvolvedBox from './components/GetInvolvedBox/GetInvolvedBox';
+/* import GetInvolvedBox from './components/GetInvolvedBox/GetInvolvedBox'; */
 
 import styles from './GetInvolved.scss';
 
 /* icons */
-import slack from './slack-icon.svg';
-import mentor from './mentor_icon.png';
+import slack from './slack_icon.png';
+/* import mentor from './mentor_icon.png'; */
 import search from './search_icon.png';
-import test from './test_icon.png';
+/* import test from './test_icon.png'; */
 
 /**
  * Constants
@@ -27,38 +27,26 @@ import test from './test_icon.png';
 const sectionTitle = 'Ready to get Involved?';
 const description = 'There are multiple ways you can get involved in the D4SD Mobility Challenge. Checkout how you can make an impact below! Details about how all the activities are building up to final submission, deadline in October, learn more about submitting by downloading the full-rules and guidelines below.';
 
-const BOX_TEXT = [
+/* const BOX_TEXT = [
   {
-    title: 'Join a Slack Discussion',
+    title: 'Explore the Challenge Briefs',
     text: ' Nunc eleifend tempus ligula, eget ullamcorper tellus faucibus at. Suspendisse ultricies sapien in nibh volutpat, et iaculis neque sodales. Duis hendrerit tellus vitae lacus accumsan vestibulum. Nulla at accumsan leo. Donec malesuada aliquet luctus. Vivamus suscipit et tortor non tempor.',
     buttonText: 'Join Slack Discussion',
-    icon: slack,
+    icon: search,
+    boxColor: '#fff',
+    textColor: '#212121',
+    button: 'globalStyles.blueOutline'
   },
   {
     title: 'Learn More',
     text: ' Nunc eleifend tempus ligula, eget ullamcorper tellus faucibus at. Suspendisse ultricies sapien in nibh volutpat, et iaculis neque sodales. Duis hendrerit tellus vitae lacus accumsan vestibulum. Nulla at accumsan leo. Donec malesuada aliquet luctus. Vivamus suscipit et tortor non tempor.',
     buttonText: 'Join Slack Discussion',
     icon: slack,
-  },
-  {
-    title: 'Become a Mentor',
-    text: ' Nunc eleifend tempus ligula, eget ullamcorper tellus faucibus at. Suspendisse ultricies sapien in nibh volutpat, et iaculis neque sodales. Duis hendrerit tellus vitae lacus accumsan vestibulum. Nulla at accumsan leo. Donec malesuada aliquet luctus. Vivamus suscipit et tortor non tempor.',
-    buttonText: 'Sign Up',
-    icon: mentor,
-  },
-  {
-    title: 'Participate in User Testing',
-    text: ' Nunc eleifend tempus ligula, eget ullamcorper tellus faucibus at. Suspendisse ultricies sapien in nibh volutpat, et iaculis neque sodales. Duis hendrerit tellus vitae lacus accumsan vestibulum. Nulla at accumsan leo. Donec malesuada aliquet luctus. Vivamus suscipit et tortor non tempor.',
-    buttonText: 'Sign Up',
-    icon: test,
-  },
-  {
-    title: 'Find a Team',
-    text: ' Nunc eleifend tempus ligula, eget ullamcorper tellus faucibus at. Suspendisse ultricies sapien in nibh volutpat, et iaculis neque sodales. Duis hendrerit tellus vitae lacus accumsan vestibulum. Nulla at accumsan leo. Donec malesuada aliquet luctus. Vivamus suscipit et tortor non tempor.',
-    buttonText: 'Join D4SD Slack',
-    icon: search,
-  },
-];
+    boxColor: '#4496FF',
+    textColor: '#fff',
+    button: 'globalStyles.orangeOutline'
+  }
+]; */
 
 /**
  * GetInvolved
@@ -100,14 +88,45 @@ function GetInvolved() {
           text={description}
         />
         <div className={styles.gridStyle}>
-          {BOX_TEXT.map(({ title, text, buttonText, icon }) => (
+          <section className={globalStyles.sectionWrapper}>
+            <div className={styles.getInvolvedBox}>
+              <div className={styles.boxWrapper}>
+                <div className={styles.boxContent}>
+                  <img src={search} alt="Explore the Challenge Briefs" />
+                  <h2>Explore the Challenge Briefs</h2>
+                  <p>Nunc eleifend tempus ligula, eget ullamcorper tellus faucibus at. Suspendisse ultricies sapien in nibh volutpat, et iaculis neque sodales. Duis hendrerit tellus vitae lacus accumsan vestibulum. Nulla at accumsan leo.</p>
+                  <div className={globalStyles.sectionBtn}>
+                    <a className={globalStyles.orangeOutline} href="">View the Briefs</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section className={globalStyles.sectionWrapperBlue}>
+            <div className={styles.getInvolvedBox}>
+              <div className={styles.boxWrapper}>
+                <div className={styles.boxContent}>
+                  <img src={slack} alt="Discuss Mobility in Slack" />
+                  <h2 className={globalStyles.white}>Discuss Mobility in Slack</h2>
+                  <p className={globalStyles.white}>As part of the IDEATION phase, we are hosting an array of discussions on our Mobility Brainstorming Prompts on the communication platform Slack. Click below to join the D4SD Slack team and get started in the 2017 Mobility Challenge!</p>
+                  <div className={globalStyles.sectionBtn}>
+                    <a className={globalStyles.whiteOutline} href="">Join D4SD Slack</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          {/* {BOX_TEXT.map(({ title, text, buttonText, icon, boxColor, textColor, button }) => (
             <GetInvolvedBox
               title={title}
               text={text}
               buttonText={buttonText}
               icon={icon}
+              boxColor={boxColor}
+              textColor={textColor}
+              button={button}
             />
-          ))}
+          ))} */}
         </div>
       </div>
       <Footer />
