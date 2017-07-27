2b6890750cbb47fb11d5deb9464a721ed95a6394
Lauren Liu <lmliu@ucsd.edu>
2017-07-27 07:04:17 -0700

    mobile navbar

--- client/components/Navbar/Navbar.jsx
+++ client/components/Navbar/Navbar.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ReactGA from 'react-ga';
 import { Link } from 'react-router';
+import classNames from 'util/classNames';
 
 import styles from './Navbar.scss';
 import landingLogo from './logo_black.png';
+
+let TimelineLite = {};
+if (process.env.browser) {
+  TimelineLite = require('gsap/TimelineLite'); // eslint-disable-line global-require
+  require('gsap/CSSPlugin'); // eslint-disable-line global-require
+}
 
 /**
  * Constants
@@ -57,26 +64,81 @@ function logClick() {
   });
 }
 
-function Navbar({ backgroundImg, headerText, subheaderText, showButton }, { router }) {
-  return (
-    <header className={styles.navbar}>
-      <div className={styles.navbarContainer}>
-        <div className={styles.logo}>
-          <Link to="/"><img className="theLogo" src={landingLogo} alt="logo" /></Link>
-        </div>
-        <nav className={styles.navbar}>
-          {LINKS.map(({ href, text }) =>
-            <Link key={text} to={href} className={(href === router.location.pathname) ? styles.active : ''}>{text}</Link>
-          )}
+class Navbar extends React.PureComponent {
+  static ANIMATION_DURATION = 0.5
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mobileNavbarOpen: false,
+    };
+  }
+
+  _mobileNavbarOnClick = () => {
+    const { mobileNavbarOpen } = this.state;
+
+    const tl = new TimelineLite()
+      /*
+      .to(this.navbar, Navbar.ANIMATION_DURATION, {
+        left: mobileNavbarOpen ? '100%' : 0,
+      }, 0)
+      */
+      .to(this.burger1, Navbar.ANIMATION_DURATION, {
+        y: mobileNavbarOpen ? 0 : 10,
+        rotation: mobileNavbarOpen ? 0 : '45deg',
+      }, 0)
+      .to(this.burger2, Navbar.ANIMATION_DURATION, {
+        width: mobileNavbarOpen ? '100%' : 0,
+        left: mobileNavbarOpen ? 0 : '50%',
+      }, 0)
+      .to(this.burger3, Navbar.ANIMATION_DURATION, {
+        y: mobileNavbarOpen ? 0 : -10,
+        rotation: mobileNavbarOpen ? 0 : '-45deg',
+      }, 0);
+
+    this.navbarItems.forEach((navbarItem, i) => {
+      tl.to(navbarItem, Navbar.ANIMATION_DURATION, {
+        left: mobileNavbarOpen ? 0 : '-100%',
+      }, i * (Navbar.ANIMATION_DURATION / 4));
+    });
+
+    this.setState({
+      mobileNavbarOpen: !mobileNavbarOpen,
+    });
+  }
+
+  render() {
+    // const { backgroundImg, headerText, subheaderText, showButton } = this.props;
+    const { router } = this.context;
+
+    return (
+      <header className={styles.navbar}>
+        <div className={styles.navbarContainer}>
+          <div className={styles.logo}>
+            <Link to="/"><img className="theLogo" src={landingLogo} alt="logo" /></Link>
+          </div>
+          <nav className={styles.navbarInner} ref={(el) => { this.navbar = el; }}>
+            {LINKS.map(({ href, text }, i) => (
+              <div className={styles.navbarLinkContainer} ref={(el) => { this.navbarItems = this.navbarItems || []; this.navbarItems[i] = el; }}>
+                <Link key={text} to={href} className={(href === router.location.pathname) ? styles.active : ''}>{text}</Link>
+              </div>
+            ))}
+          </nav>
           <div className={styles.registerButton}>
             <div className={styles.buttonContainer}>
-              <a onClick={logClick} href="/getinvolved">Get Involved</a>
+              <Link onClick={logClick} to="/getinvolved">Get Involved</Link>
             </div>
           </div>
-        </nav>
-      </div>
-    </header>
-  );
+          <button className={styles.mobileNavbar} onClick={this._mobileNavbarOnClick}>
+            <div className={classNames([styles.burger, styles.burger1])} ref={(el) => { this.burger1 = el; }} />
+            <div className={classNames([styles.burger, styles.burger2])} ref={(el) => { this.burger2 = el; }} />
+            <div className={classNames([styles.burger, styles.burger3])} ref={(el) => { this.burger3 = el; }} />
+          </button>
+        </div>
+      </header>
+    );
+  }
 }
 
 Navbar.propTypes = propTypes;
