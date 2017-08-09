0ccb9a4cdf137c07c10b7b69848fae4ef0c21003
Lauren Liu <lmliu@ucsd.edu>
2017-08-09 09:50:37 -0700

    favicon and scale logo updates

--- client/modules/App/App.jsx
+++ client/modules/App/App.jsx
@@ -21,41 +21,32 @@ function App({ children }) {
     <div>
       <Helmet
         titleTemplate="%s - Design for San Diego"
-        meta={[
-          { charset: 'utf-8' },
-          { 'http-equiv': 'x-ua-compatible', content: 'ie=edge' },
-          { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-          { name: 'keywords', content: '' },
-          { name: 'author', content: '' },
-          { name: 'robots', content: 'index, follow' },
-          { name: 'revisit-after', content: '1 days' },
-          { name: 'theme-color', content: '#2d324d' },
-          { name: 'ms-application-TileColor', content: 'FFFFFF' },
-          { name: 'ms-application-TileImage', content: 'mstile-144x144.png' },
-          { name: 'ms-application-square70x70logo', content: 'mstile-70x70.png' },
-          { name: 'ms-application-square150x150logo', content: 'mstile-150x150.png' },
-          { name: 'ms-application-wide310x150logo', content: 'mstile-310x150.png' },
-          { name: 'ms-application-square310x310logo', content: 'mstile-310x310.png' },
-        ]}
-        link={[
-          { rel: 'author', href: '/humans.txt' },
-          { rel: 'apple-touch-icon-precomposed', sizes: '57x57', href: '/apple-touch-icon-57x57.png' },
-          { rel: 'apple-touch-icon-precomposed', sizes: '114x114', href: '/apple-touch-icon-114x114.png' },
-          { rel: 'apple-touch-icon-precomposed', sizes: '72x72', href: '/apple-touch-icon-72x72.png' },
-          { rel: 'apple-touch-icon-precomposed', sizes: '144x144', href: '/apple-touch-icon-144x144.png' },
-          { rel: 'apple-touch-icon-precomposed', sizes: '60x60', href: '/apple-touch-icon-60x60.png' },
-          { rel: 'apple-touch-icon-precomposed', sizes: '120x120', href: '/apple-touch-icon-120x120.png' },
-          { rel: 'apple-touch-icon-precomposed', sizes: '76x76', href: '/apple-touch-icon-76x76.png' },
-          { rel: 'apple-touch-icon-precomposed', sizes: '152x152', href: '/apple-touch-icon-152x152.png' },
-          { rel: 'icon', type: 'image/png', sizes: '196x196', href: '/favicon-196x196.png' },
-          { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
-          { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-          { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
-          { rel: 'icon', type: 'image/png', sizes: '128x128', href: '/favicon-128x128.png' },
-          { rel: 'manifest', href: '/manifest.json' },
-          { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#2d324d' },
-        ]}
-      />
+      >
+        {/* Meta */}
+        <html lang="en" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta content="ie=edge" httpEquiv="x-ua-compatible" />
+        {/* Generic SEO */}
+        <meta name="author" content="index, follow" />
+        <meta name="keywords" content="1 days" />
+        <meta />
+        {/* http://robotstxt.org/ */}
+        <meta name="robots" content="" />
+        <meta name="revisit-after" content="" />
+        {/* http://humanstxt.org/ */}
+        <link rel="author" href="humans.txt" />
+        {/* https://realfavicongenerator.net/ */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=Nm5EJq5nq2" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=Nm5EJq5nq2" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=Nm5EJq5nq2" />
+        <link rel="manifest" href="/manifest.json?v=Nm5EJq5nq2" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg?v=Nm5EJq5nq2" color="#5bbad5" />
+        <link rel="shortcut icon" href="/favicon.ico?v=Nm5EJq5nq2" />
+        <meta name="apple-mobile-web-app-title" content="D4SD" />
+        <meta name="application-name" content="D4SD" />
+        <meta name="theme-color" content="#ffffff" />
+      </Helmet>
       {children}
     </div>
   );
