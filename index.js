88acd219b9a7e1ab746662eceab248cb1b7bb851
skyhawk5407 <vincentchu5407@gmail.com>
2019-08-27 11:03:47 -0700

    Prep for merge

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
  */
 
 if (['staging', 'production'].includes(process.env.NODE_ENV)) {
-  process.env.webpackAssets = JSON.stringify(require('./client-dist/manifest.json'));
-  process.env.webpackChunkAssets = JSON.stringify(require('./client-dist/chunk-manifest.json'));
+  process.env.webpackAssets = JSON.stringify(require('./client-dist/manifest.json.js'));
+  process.env.webpackChunkAssets = JSON.stringify(require('./client-dist/chunk-manifest.json.js'));
 
-  require('./server-dist/server.bundle.js');
+  require('./server-dist/server.bundle.js.js');
 } else {
   require('babel-register')({
     plugins: [
